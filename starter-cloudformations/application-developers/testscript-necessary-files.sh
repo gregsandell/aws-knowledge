fdfb1e48457e293ef0b44820ded368a738f5f6b3
Greg Sandell <gsandell_99@yahoo.com>
2025-11-27 21:38:51 -0800

    Introduce bash variables; add and test put-group-policy and delete-group-policy

--- starter-cloudformations/application-developers/testscript-necessary-files.sh
+++ starter-cloudformations/application-developers/testscript-necessary-files.sh
@@ -2,75 +2,97 @@
 # TODO Rewrite this using variables and loop
 #
 # The purpose of this script is to test that a number of AWS actions/commands are permitted
-# for the gjsWebsiteAdmin user. If any of the commands fail, an error message for each of them
+# for as given user. If any of the commands fail, an error message for each of them
 # is printed to stdout.
-#
-# The script depends on this policy to exist:
-#   arn:aws:iam::975720571991:policy/all-the-customer-managed-policies-final
 
-# The group "testscript-temp-group" is created temporarily here and then deleted in this script.
-# This is so that we can test creating and then deleting a group.
+STARTER_POLICY=arn:aws:iam::975720571991:policy/starter-policies # Must be pre-existing
+GJS_USER=gjsWebsiteAdmin # Must be pre-existing
+CLI_CAPABIILITIES_GROUP=aws-cli-capabilities # Must be pre-existing
+TEMP_GROUP=testscript-temp-group # Created and deleted in this script
 
-
-aws iam list-groups-for-user --user-name gjsWebsiteAdmin > /dev/null
+aws iam list-groups-for-user --user-name $GJS_USER > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed list-groups-for-user"
 fi
 
-aws iam list-attached-user-policies --user-name gjsWebsiteAdmin > /dev/null
+aws iam list-attached-user-policies --user-name $GJS_USER > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed list-attached-user-policies"
 fi
-  aws iam list-access-keys --user-name gjsWebsiteAdmin > /dev/null
+  aws iam list-access-keys --user-name $GJS_USER > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed list-access-keys"
 fi
-  aws iam list-user-policies --user-name gjsWebsiteAdmin  > /dev/null
+  aws iam list-user-policies --user-name $GJS_USER  > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed list-user-policies"
 fi
-  aws iam get-user --user-name gjsWebsiteAdmin  > /dev/null
+  aws iam get-user --user-name $GJS_USER  > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed get-user"
 fi
-  aws iam get-group --group-name aws-cli-capabilities  > /dev/null
+  aws iam get-group --group-name $CLI_CAPABIILITIES_GROUP  > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed get-group"
 fi
 
-  aws iam get-policy --policy-arn arn:aws:iam::975720571991:policy/all-the-customer-managed-policies-final > /dev/null
+  aws iam get-policy --policy-arn $STARTER_POLICY > /dev/null
   if [ $? -ne 0 ]; then
     echo "Failed get-policy"
   fi
 
 # Here is the test for create-group, and it does double-duty as creating a temporary
 # resource for the delete-group test.
-  aws iam create-group --group-name testscript-temp-group  > /dev/null
+  aws iam create-group --group-name $TEMP_GROUP  > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed create-group"
 fi
 
+# TODO Create necessary resources and edit the test below to get it working
+aws iam put-group-policy \
+  --group-name $TEMP_GROUP \
+  --policy-name AllowListGroupsForUser \
+  --policy-document '{
+    "Version":"2012-10-17",
+    "Statement":[{"Effect":"Allow","Action":"iam:ListGroupsForUser","Resource":"*"}]
+  }'
+if [ $? -ne 0 ]; then
+  echo "Failed put-group-policy"
+fi
+
+aws iam delete-group-policy \
+  --group-name $TEMP_GROUP \
+  --policy-name AllowListGroupsForUser
+if [ $? -ne 0 ]; then
+  echo "Failed delete-group-policy"
+fi
+
 # Delete the temporary group
-  aws iam delete-group --group-name testscript-temp-group  > /dev/null
+  aws iam delete-group --group-name $TEMP_GROUP  > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed delete-group"
 fi
 
-aws iam list-attached-group-policies --group-name aws-cli-capabilities > /dev/null
+aws iam list-attached-group-policies --group-name $CLI_CAPABIILITIES_GROUP > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed list-attached-group-policies"
 fi
-aws iam list-group-policies --group-name aws-cli-capabilities > /dev/null
+aws iam list-group-policies --group-name $CLI_CAPABIILITIES_GROUP > /dev/null
 if [ $? -ne 0 ]; then
   echo "Failed list-group-policies"
 fi
 
+
+
 # TODO Create necessary resources and edit the test below to get it working
-# Probably need to add create-group-policy policy first
+# Probably need to add put-group-policy policy first
 # Then create a throwaway policy to attach to the group for testing
 # Then probably add a delete-policy policy, then delete the throwaway policy after testing
 #aws iam detach-group-policy \
 #    --group-name MyDeveloperGroup \
 #    --policy-arn arn:aws:iam::aws:policy/PowerUserAccess
 
-
+# TODO Create necessary resources and edit the test below to get it working
+#aws iam get-group-policy \
+#  --group-name foo-group \
+#  --policy-name AllowListGroupsForUser
